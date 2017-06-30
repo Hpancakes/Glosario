616f7c85b2f68f307d8c872f01fad522b0a7a5cb
Taslot <jluis.larav@gmail.com>
2017-06-30 14:14:55 -0500

    Mas palabras

--- Glosario/ViewController.swift
+++ Glosario/ViewController.swift
@@ -54,35 +54,65 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func makeDefinition() -> [DefinitionsPND] {
         let word1 = DefinitionsPND()
         word1.word = "A-scan display"
-        word1.category = "UT"
-        word1.definition = "A display in which the received signal amplitude is shown as a vertical excursion from the horizontal sweep time  trace. The horizontal distance between any two signals represents the material distance between the two conditions causing the signals. 10 In a linear system, the vertical excursion is proportional to the amplitude of the signal."
+        word1.category = "Ultrasonido industrial"
+        word1.definition = "Visualización en la que la amplitud de la señal recibida se muestra como una excursión vertical a partir de la trayectoria del tiempo de barrido horizontal La distancia horizontal entre dos señales representa la distancia del material entre las dos condiciones que causan las señales 10 En un sistema lineal , La excursión vertical es proporcional a la amplitud de la señal."
         
         let word2 = DefinitionsPND()
-        word2.word = "absolute coil"
-        word2.category = "EC"
-        word2.definition = "A coil of electrically conductive wire that responds to the electromagnetic properties of that region of the test object that is within the magnetic field of the coil, without comparison to the response of a second coil at a different location on the same or similar material."
+        word2.word = "Absolute coil"
+        word2.category = "Corrientes Eddy"
+        word2.definition = "Una bobina de alambre eléctricamente conductor que responde a las propiedades electromagnéticas de esa región del objeto de prueba que está dentro del campo magnético de la bobina, sin comparación con la respuesta de una segunda bobina en un lugar diferente en el mismo material o material similar."
         
         let word3 = DefinitionsPND()
-        word3.word = "absolute measurement"
-        word3.category = "EC"
-        word3.definition = "Measurement made with an absolute coil"
+        word3.word = "Absolute measurement"
+        word3.category = "Correintes Eddy"
+        word3.definition = "Medida realizada con una bobina absoluta"
         
         let word4 = DefinitionsPND()
-        word4.word = "absolute pressure"
+        word4.word = "Absolute pressure"
         word4.category = "General"
-        word4.definition = "Pressure above absolute zero value, or pressure above that of empty space. Equal to sum of local atmospheric pressure and gage atmosphere."
+        word4.definition = "Presión por encima del valor cero absoluto, o presión por encima de la del espacio vacío. Igual a la suma de la presión atmosférica local y la atmósfera de calibración."
         
         let word5 = DefinitionsPND()
-        word5.word = "absolute temperature"
+        word5.word = "Absolute temperature"
         word5.category = "General"
-        word5.definition = "Temperature measured from absolute zero temperature, expressed in Kelvin (K) in SI."
+        word5.definition = "Temperatura medida desde la temperatura cero absoluta, expresada en Kelvin (K) en SI."
         
         let word6 = DefinitionsPND()
-        word6.word = "absorbed dose"
-        word6.category = "RT"
-        word6.definition = "The amount of energy imparted to matter by an ionizing particle per unit mass of irradiated material at the place of interest. Absorbed dose is expressed in Gray (Gy) or rads."
+        word6.word = "Absorbed dose"
+        word6.category = "Radiografia Tradicional"
+        word6.definition = "La cantidad de energía impartida a la materia por una partícula ionizante por unidad de masa de material irradiado en el lugar de interés. La dosis absorbida se expresa en Gray (Gy) o rads."
         
-        return [word1, word2, word3, word4, word5, word6]
+        let word7 = DefinitionsPND()
+        word7.word = "Absorption coefficient, lineal"
+        word7.category = "Ultrasonido industrial"
+        word7.definition = "La disminución fraccionada en la intensidad transmitida por unidad de espesor del absorbedor. Por lo general se designa con el símbolo μ y se expresa en unidades de cm -1"
+        
+        let word8 = DefinitionsPND()
+        word8.word = "Acceptable quality level (AQL)"
+        word8.category = "General"
+        word8.definition = "El porcentaje máximo defectuoso (o el número máximo de unidades con anomalías rechazables por cien unidades) que, a efectos de las pruebas de muestreo, puede considerarse satisfactorio como un promedio de proceso."
+        
+        let word9 = DefinitionsPND()
+        word9.word = "Acceptance criteria"
+        word9.category = "General"
+        word9.definition = "La norma con la que deben compararse los resultados de las pruebas con el fin de establecer la aceptabilidad funcional de una parte o sistema que se esté examinando."
+        
+        let word10 = DefinitionsPND()
+        word10.word = "Acceptance level"
+        word10.category = "General"
+        word10.definition = "Un nivel de prueba por encima o por debajo del cual los objetos de prueba son aceptables en contraste con el nivel de rechazo."
+        
+        let word11 = DefinitionsPND()
+        word11.word = "Acceptance standard"
+        word11.category = "General"
+        word11.definition = "Un espécimen similar al objeto de ensayo que contiene discontinuidades naturales o artificiales que están bien definidas y son similares en tamaño o extensión al máximo aceptable en el producto. Ver norma de referencia y estándar."
+        
+        let word12 = DefinitionsPND()
+        word12.word = "Acceptance standard"
+        word12.category = "General"
+        word12.definition = "Un espécimen similar al objeto de ensayo que contiene discontinuidades naturales o artificiales que están bien definidas y son similares en tamaño o extensión al máximo aceptable en el producto. Ver norma de referencia y estándar."
+        
+        return [word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11,   ]
     }
 
 
