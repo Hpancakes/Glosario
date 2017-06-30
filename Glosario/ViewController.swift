412cbdf283add8dd256fa01636c9edea441cc948
Taslot <jluis.larav@gmail.com>
2017-06-30 12:18:18 -0500

    Now with silly arrays

--- Glosario/ViewController.swift
+++ Glosario/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var wordTableView: UITableView!
+    
+    var def = ["beef","chiken","dog"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        wordTableView.dataSource = self
+        wordTableView.delegate = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return def.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = def[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
